55dce3dcf78923c19bfb873bf2eddf9bb980e993
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-12-24 12:04:25 +0900

    (2024.12.23) Setup

---
+++ Project_Data/Assets/Scripts/Data/Class/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game ItemData", menuName = "SO/ItemData")]
+
+public class ItemData : ScriptableObject
+{
+    public string name;
+}
---
+++ Project_Data/Assets/Scripts/GameData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.Rendering;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Game DataBase", menuName = "GameData/Database")]
+
+public class GameData : ScriptableObject
+{
+
+
+
+}

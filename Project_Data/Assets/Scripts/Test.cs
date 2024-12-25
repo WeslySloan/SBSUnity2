1ca5928971a5a7189bfcea931b68d8736e363719
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-12-25 21:14:44 +0900

    (2024.12.24) 아이템 슬롯 기능 구현

--- Project_Data/Assets/Scripts/Test.cs
+++ Project_Data/Assets/Scripts/Test.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debugger.Log("Hello Unity");
     }
 }

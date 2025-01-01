4f9f31fb14bc03a02186039277e8591d8b054431
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-01-01 17:38:42 +0900

    (2024.12.31)

--- Project_Dungreed/Assets/Script/PlayerController.cs
+++ Project_Dungreed/Assets/Script/PlayerController.cs
@@ -4,24 +4,77 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float moveSpeed = 5.0f;
-        private Vector3 moveDirection = Vector3.zero;
+    public float max_speed;
+    public float jump_power;
+    bool is_jump = false;
 
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        // 키 떼면 멈춤
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
+        }
+        // 점프
 
-        float y = Input.GetAxis("Vertical");  // 수직이동
+        if (Input.GetButtonDown("Jump") && !is_jump)
+        {
+            rigid.AddForce(Vector2.up * jump_power, ForceMode2D.Impulse);
+            is_jump = true;
+        }
 
-        moveDirection = new Vector3 (x, y, 0);
 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        // 걷기 애니메이션
+        animator.SetInteger("Horizon", (int)Input.GetAxisRaw("Horizontal"));
+        if (animator.GetInteger("Horizon") == -1)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (animator.GetInteger("Horizon") == 1)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        // 점프 애니메이션
+        animator.SetBool("IsJump", is_jump);
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        // 이동
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > max_speed)
+            rigid.velocity = new Vector2(max_speed, rigid.velocity.y);
+        if (rigid.velocity.x < max_speed * (-1))
+            rigid.velocity = new Vector2(max_speed * (-1), rigid.velocity.y);
+
+
+        // 레이 캐스트(2단점프 방지)
+        if (rigid.velocity.y < 0 && is_jump)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayhit = Physics2D.Raycast(rigid.position, Vector3.down, 0.5f, LayerMask.GetMask("PlatForm"));
+
+            if (rayhit.collider != null)
+            {
+                is_jump = false;
+            }
+        }
     }
 }
